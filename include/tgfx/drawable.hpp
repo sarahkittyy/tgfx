e5c40d534c7655657bf0d158f6860f640d1aae28
sarahkittyy <sarahohlin1@gmail.com>
2019-08-15 15:47:27 -0400

    base drawable class code

---
+++ include/tgfx/drawable.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "tgfx/screen.hpp"
+
+namespace tgfx
+{
+
+/**
+ * @brief Inheritable drawable class. Passed to tgfx::context for rendering to the screen.
+ * 
+ */
+class drawable
+{
+public:
+	/// Virtual DTor for inheritance
+	virtual ~drawable();
+
+protected:
+	/// Called by the context in context.draw(). Inherit to define rendering to the screen.
+	virtual void draw(screen& scr) const = 0;
+
+	// For context to call this->draw()
+	friend class screen;
+};
+
+}
