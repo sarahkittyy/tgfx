ef1f30c5a385ea349a5e7f2d80d4b6727fd5e961
sarahkittyy <sarahohlin1@gmail.com>
2019-08-15 18:31:15 -0400

    added ansi-color tests

--- test/tgfx-test/internal/ansi-color.hpp
+++ test/tgfx-test/internal/ansi-color.hpp
@@ -1,29 +1,44 @@
 #pragma once
 
 #include "gtest/gtest.h"
+#include "tgfx/color.hpp"
 #include "tgfx/internal/ansi-color.hpp"
 
-#undef COLOR_MODE_TRUE
-#undef COLOR_MODE_256
-#undef COLOR_MODE_16
+#ifdef COLOR_MODE_16
 
 TEST(ansi_color, MODE_16)
 {
-#define COLOR_MODE_16
 	using namespace tgfx::internal;
+	using tgfx::color;
 	EXPECT_EQ(get_color_escape_code(color()), "\u001b[30m");
-#undef COLOR_MODE_16
+	EXPECT_EQ(get_color_escape_code(color(128, 128, 128)), "\u001b[1;40m");
+	EXPECT_EQ(get_color_escape_code(color(), false), "\u001b[30m");
+	EXPECT_EQ(get_color_escape_code(color(128, 128, 128), false), "\u001b[1;40m");
+
+	EXPECT_EQ(get_color_escape_code(color(255, 255, 255)), "\u001b[1;37m");
+	EXPECT_EQ(get_color_escape_code(color(192, 192, 192)), "\u001b[37m");
+	EXPECT_EQ(get_color_escape_code(color(255, 255, 255), false), "\u001b[1;47m");
+	EXPECT_EQ(get_color_escape_code(color(192, 192, 192), false), "\u001b[47m");
 }
 
-TEST(ansi_color, MODE_256){
-#define COLOR_MODE_256
+#elif COLOR_MODE_256
 
-#undef COLOR_MODE_256
+TEST(ansi_color, MODE_256)
+{
+	using namespace tgfx::internal;
+	using tgfx::color;
+
+	EXPECT_EQ(get_color_escape_code(color(0x880000ff)), "\u001b[38;5;88 m");
+	EXPECT_EQ(get_color_escape_code(color(0x87af5eff)), "\u001b[38;5;107 m");
+	EXPECT_EQ(get_color_escape_code(color(0xaf0000ff)), "\u001b[38;5;124 m");
+	EXPECT_EQ(get_color_escape_code(color(0x880000ff), false), "\u001b[48;5;88 m");
+	EXPECT_EQ(get_color_escape_code(color(127, 127, 255), false), "\u001b[48;5;88 m");
 }
+
+#elif COLOR_MODE_TRUE
 
 TEST(ansi_color, MODE_TRUE)
 {
-#define COLOR_MODE_TRUE
-
-#undef COLOR_MODE_TRUE
 }
+
+#endif
