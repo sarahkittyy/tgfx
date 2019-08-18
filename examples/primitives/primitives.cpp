998ea5bbfef7685c6955fa854b586add79c3e66b
sarahkittyy <sarahohlin1@gmail.com>
2019-08-18 14:37:18 -0400

    added primitives example

--- examples/primitives/primitives.cpp
+++ examples/primitives/primitives.cpp
@@ -2,5 +2,21 @@
 
 int main()
 {
-	//TODO: implement primitives example
+	tgfx::context ctx;
+
+	// Draw a line from the top-left corner to the bottom-right.
+	tgfx::line line(tgfx::point({ 0, 0 }, tgfx::color(0, 0, 0)),
+					tgfx::point(ctx.size(), tgfx::color(255, 255, 255)));
+
+	while (ctx.open())
+	{
+		ctx.clear();
+
+		// Render the line.
+		ctx.draw(line);
+
+		ctx.render();
+	}
+
+	return 0;
 }
