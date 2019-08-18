e0d16104a0d05c6c443b512704f12172b6f10596
sarahkittyy <sarahohlin1@gmail.com>
2019-08-18 13:42:55 -0400

    added point render testing

--- test/tgfx-test/primitive/point.hpp
+++ test/tgfx-test/primitive/point.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "tgfx/color.hpp"
 #include "tgfx/primitive/point.hpp"
+#include "tgfx/screen.hpp"
 #include "tgfx/util/vec2.hpp"
 
 TEST(point, Point)
@@ -14,4 +16,15 @@ TEST(point, Point)
 	p.set_position(tgfx::vec2u(20, 15));
 	EXPECT_EQ(p.get_position(), tgfx::vec2u(20, 15));
 	EXPECT_EQ(p.get_color(), tgfx::color(127, 127, 255));
+}
+
+TEST(point, Render)
+{
+	using namespace ::testing;
+	tgfx::screen s;
+
+	tgfx::point p(tgfx::vec2u(0, 0), tgfx::color(0, 0, 0));
+	s.draw(p);
+
+	EXPECT_THAT(s.flush(), HasSubstr("\u001b[48;2;0;0;0 m "));
 }
